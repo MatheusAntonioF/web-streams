30637937359f5e1d8a27e5200a0b3b078e93957b
Matheus Felipe Antonio <matheusantoniof@gmail.com>
2023-01-17 09:43:23 -0300

    feat: show data from stream in HTML

--- app/index.js
+++ app/index.js
@@ -1,23 +1,82 @@
 const API_URL = 'http://localhost:3000';
+let counter = 0;
 
 async function consumeAPI(signal) {
   const response = await fetch(API_URL, {
     signal,
   });
 
-  let counter = 0;
-
-  const reader = response.body.pipeThrough(new TextDecoderStream()).pipeTo(
-    new WritableStream({
-      write(chunk) {
-        console.log(counter++, '🚀 ~ chunk', chunk);
-      },
-    })
-  );
+  const reader = response.body
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(parseNDJSON());
 
   return reader;
 }
 
-const abortController = new AbortController();
+function appendToHTML(element) {
+  return new WritableStream({
+    write({ title, description, url_anime }) {
+      const card = `
+        <article>
+          <div class="text">
+            <h3>[${++counter}] ${title}</h3>
 
-await consumeAPI(abortController.signal);
+            <p>
+              ${description.slice(0, 100)}
+            </p>
+
+            <a href="${url_anime}">Here's why</a>
+          </div>
+        </article>
+      `;
+
+      element.innerHTML += card;
+    },
+    abort(reason) {
+      console.log('aborted', reason);
+    },
+  });
+}
+
+// parse if more than one chunk arrives in one broadcast
+function parseNDJSON() {
+  let ndjsonBuffer = '';
+
+  return new TransformStream({
+    transform(chunk, controller) {
+      ndjsonBuffer += chunk;
+
+      const items = ndjsonBuffer.split('\n');
+
+      items.slice(0, -1).forEach(item => controller.enqueue(JSON.parse(item)));
+
+      ndjsonBuffer = items[items.length - 1];
+    },
+    // event trigged when the transform process finish
+    flush(controller) {
+      if (!ndjsonBuffer) return;
+
+      controller.enqueue(JSON.parse(ndjsonBuffer));
+    },
+  });
+}
+
+const [start, stop, cards] = ['start', 'stop', 'cards'].map(item =>
+  document.getElementById(item)
+);
+
+let abortController = new AbortController();
+
+start.addEventListener('click', async () => {
+  const readable = await consumeAPI(abortController.signal);
+
+  readable.pipeTo(appendToHTML(cards));
+});
+
+stop.addEventListener('click', () => {
+  abortController.abort();
+
+  console.log('aborting');
+
+  abortController = new AbortController();
+});
